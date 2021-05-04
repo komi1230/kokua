44a1fb4b57aeaae4c3aaf71b842aa99931c5451c
komi1230 <yukke.konan@gmail.com>
2021-05-04 15:28:31 +0900

    Update

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # launcher
-Yet Another Embulk written in Rust
+
+Yet Another Embulk written in Rust.
+
+This project is still not completed and you cannot use.
--- src/bin/kokua/main.rs
+++ src/bin/kokua/main.rs
@@ -1,18 +1,3 @@
-mod help;
-
 fn main() {
-    help::help();
-}
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn echo() {
-        assert!(true);
-    }
-
-    #[test]
-    fn sample() {
-        assert!(true);
-    }
+    println!("hello");
 }
