b8a750d95632a961b1c06fddb2c02ddf24e5e5df
komi1230 <yukke.konan@gmail.com>
2021-05-20 00:15:36 +0900

    Fix

--- src/bin/kokua/initialize.rs
+++ src/bin/kokua/initialize.rs
@@ -1,9 +1,8 @@
-use std::path::{Path, PathBuf};
+use std::path::PathBuf;
 
 fn config_dir() -> PathBuf {
     let mut path = dirs::home_dir().expect("Environment varialbe $HOME should be set.");
-    path.push(".config");
-    path.push("kokua");
+    path.push(".kokua");
     path
 }
 
@@ -12,8 +11,13 @@ pub fn exec() {
     let path = path_buf.as_path();
     std::fs::create_dir_all(path).expect("Something wrong in creating config dir.");
 
+    // Create bin dir
+    let mut path_bin = path.to_path_buf();
+    path_bin.push("bin");
+    std::fs::create_dir_all(path_bin).unwrap();
+
     // Create plugins dir
-    let mut path = path.to_path_buf();
-    path.push("plugins");
-    std::fs::create_dir(path);
+    let mut path_plugins = path.to_path_buf();
+    path_plugins.push("plugins");
+    std::fs::create_dir_all(path_plugins).unwrap();
 }
